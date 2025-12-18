c58f59b12394316b080986192daa5b0c762e48fc
IwkooO <iwo.godzwon@gmail.com>
2025-12-18 17:49:31 +0100

    Change: Normalization to projection layer in sparsity fixes all

--- src/cbm/models/__init__.py
+++ src/cbm/models/__init__.py
@@ -4,12 +4,20 @@ from src.cbm.models.query_sparse import (
     ConceptHeadQuerySparse,
     CBM_QuerySparse,
     PatchMixer,
-    sparsemax,
+)
+from src.cbm.models.query_topk_256 import (
+    ConceptHeadQueryTopK,
+    CBM_QueryTopK,
+    ConceptHeadQueryTopK256,
+    CBM_QueryTopK256,
 )
 
 __all__ = [
     "ConceptHeadQuerySparse",
     "CBM_QuerySparse",
     "PatchMixer",
-    "sparsemax",
+    "ConceptHeadQueryTopK",
+    "CBM_QueryTopK",
+    "ConceptHeadQueryTopK256",
+    "CBM_QueryTopK256",
 ]
